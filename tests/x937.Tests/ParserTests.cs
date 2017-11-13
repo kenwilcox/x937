2e94095e4131611daa5d34e5905228ba7fec96e5
Ken Wilcox <wilcoxk@gmail.com>
2017-11-13 10:22:20 -0700

    Added ParserTests, need to create additional test files

---
+++ tests/x937.Tests/ParserTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace x937.Tests
+{
+    public class ParserTests
+    {
+        [Fact]
+        public void TestThat_ParsingACorrectFile_Works()
+        {
+            // Arrange
+            var file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"101Bank Of America20130218.ICL");
+
+            // Act
+            var recs = Parser.ParseX9File(file);
+
+            // Assert
+            Assert.Equal(21, recs.Count);
+        }
+
+        // TODO Generate additional test files that are broken...
+    }
+}
